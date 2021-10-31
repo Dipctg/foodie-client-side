b325d29de344d12953f41606157a8647dd5a3d3b
Dipctg <dip1995.bd@gmail.com>
2021-10-31 18:33:08 +0600

    Four-2

--- src/Component/Events/Events.js
+++ src/Component/Events/Events.js
@@ -7,7 +7,7 @@ const Events = () => {
         <div>
             <h1>Friday spacial Events </h1>
             <div className='bg-purple-300 p-4'>
-                <Link as={HashLink} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 p-2" to="/packages">Packages</Link>
+                <Link as={HashLink} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 " to="/packages">Packages</Link>
                 <br />
                 <Link as={HashLink} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" to="/breakfast">Breakfast</Link>
                 <br />
